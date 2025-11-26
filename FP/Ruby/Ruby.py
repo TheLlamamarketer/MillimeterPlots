aad99150c2c0b2c658c453e3c1fb133f11511726
TheLlamamarketer <98122020+TheLlamamarketer@users.noreply.github.com>
2025-11-26 22:34:23 +0100

    basically finished Ruby

--- FP/Ruby/Ruby.py
+++ FP/Ruby/Ruby.py
@@ -51,7 +51,8 @@ for file in sorted(source_dir.iterdir()):
         't': t, 
         'A': A, 
         'header': [start, step0, direction, Ratio],
-        'lambda': Offset + Ratio * (start + direction * step0 * t)
+        #'lambda': Offset + Ratio * (start + direction * step0 * t),
+        'lambda': start + step0 * direction * t
     }
 
 # --------------------------------------------------------------------------------
@@ -102,53 +103,80 @@ def find_Hg_peaks(lam_Hg, A_Hg, Hg_lines):
         
         line_peaks.append((b_val, props_hg['peak_heights'][i]))
 
-
-    hg_start, hg_step, hg_direction, Ratio = data["Hg_lines"]["header"]
     lambda_peaks = np.array([peak[0] for peak in line_peaks])
     
-    t_hg_peaks = ((lambda_peaks-Start_corrected)/Ratio + 350 - hg_start)/(hg_direction * hg_step)
-    #t_hg_peaks = data["Hg_lines"]["t"][peaks_hg]
+    results_line_hg = lmfit(xdata=lambda_peaks, ydata=Hg_lines, model="linear")
+    
+    def get_cov(result, p1, p2):
+        names = list(result.params.keys())
+        i = names.index(p1)
+        j = names.index(p2)
+        return result.covar[i, j]
+    
+    cov_ab = get_cov(results_line_hg, 'a', 'b')
 
 
+    s_cal = []
+    for result_Hg, lambda_peak in zip(results_Hg_y, lambda_peaks):
+        sa = results_line_hg.params['a'].stderr
+        sb = results_line_hg.params['b'].stderr
+        slam_nom = result_Hg.params['b'].stderr
+        b = results_line_hg.params['b'].value
+
+        var = (sa**2
+            + (lambda_peak**2) * sb**2
+            + (b**2) * slam_nom**2
+            + 2 * lambda_peak * cov_ab)
+        s_cal.append(np.sqrt(var))
+        
     data_hg = {
         'linelist': Hg_lines,
-        'fitted': lambda_peaks,
+        'fitted':  lambda_peaks,
         'dfitted': [result_Hg.params['b'].stderr for result_Hg in results_Hg_y],
-        'diff': lambda_peaks - Hg_lines,
+        'calibrated': results_line_hg.eval(x=lambda_peaks),
+        'diff': Hg_lines - results_line_hg.eval(x=lambda_peaks),
+        'dcalibrated': s_cal,
     }
 
     headers_hg = {
-        'linelist':     {'label': '{${Hg}_{lines}/nm$}', 'intermed': True },
-        'fitted':       {'label': '{${Hg}_{fit}/nm$}', 'intermed': True, 'err': data_hg['dfitted']},
-        'diff':         {'label': '{$\\Delta Hg/nm$}', 'intermed': True},
+        'linelist':     {'label': '{$\\lambda_{real}/nm$}', 'intermed': True },
+        'fitted':       {'label': '{$\\lambda_{nom}/nm$}', 'intermed': False, 'err': data_hg['dfitted']},
+        'calibrated':   {'label': '{$\\lambda_{fit}/nm$}', 'intermed': True, 'err': data_hg['dcalibrated']},
+        'diff':         {'label': '{$\\lambda_{real} - \\lambda_{fit} /nm$}', 'intermed': True},
     }
 
     print_standard_table(
         data_hg,
         headers_hg,
-        caption="Fitted Hg line positions and their differences to the known literature values.",
+        caption="Literaturwerte $\\lambda_{real}$ und gemessene Wellenlängen $\\lambda_{nom}$ der Quecksilberlinien sowie deren kalibrierte Werte $\\lambda_{fit}$ und Differenzen.",
         label="tab:ruby_hg_lines",
         show=True
     )
-    print(f"RMS of differences: {np.sqrt(np.mean((lambda_peaks - Hg_lines)**2)):.3g} nm")
+    
+    print(f"RMS of differences: {np.sqrt(np.mean((results_line_hg.eval(x=lambda_peaks) - Hg_lines)**2)):.4g} nm")
+    
+    print(results_line_hg.fit_report())
+    print(f"a = {print_round_val(results_line_hg.params['a'].value, results_line_hg.params['a'].stderr)} nm, b = {print_round_val(results_line_hg.params['b'].value, results_line_hg.params['b'].stderr)}")
 
-    results_line_hg = lmfit(xdata=t_hg_peaks, ydata=Hg_lines, model="linear")
+
+
 
     fighg, axhg = plot_data(
-        [DatasetSpec(x=t_hg_peaks, y=Hg_lines, label='Hg Lines from NIST', color='tab:blue', marker='x', fit_x=t_hg_peaks, fit_y=results_line_hg.eval(x=t_hg_peaks), fit_label='Linear Fit', fit_color='tab:cyan')],
+        [DatasetSpec(x=lambda_peaks, y=Hg_lines, label='Hg Lines from NIST', color='tab:blue', marker='o', fit_x=lambda_peaks, fit_y=results_line_hg.eval(x=lambda_peaks), fit_label='Linear Fit', fit_color='tab:cyan')],
         title=f'Hg Line Calibration',
-        ylabel='Wavelength (nm)',
-        xlabel='Time t/s',
-        legend_position='center left',
+        ylabel='$\\lambda_{real}$ (nm)',
+        xlabel='$\\lambda_{nom}$ (nm)',
+        legend_position=(0.02, 0.65),
         color_seed=89,
         plot='figure',
     )
     
     axhg.tick_params(axis='y', labelcolor='tab:blue')
-    
+
     ax2 = axhg.twinx()
+    ax2.axhline(0, color='tab:orange', linestyle='--', label='linear fit')
     ax2.set_ylabel('Fit Residuals (nm)', color='tab:orange')
-    ax2.plot(t_hg_peaks, results_line_hg.eval(x=t_hg_peaks) - Hg_lines, 'x', label='Fit Residuals', color='tab:orange')
+    ax2.plot(lambda_peaks, results_line_hg.eval(x=lambda_peaks) - Hg_lines, 'x', label='Fit Residuals', color='tab:orange')
     ax2.tick_params(axis='y', labelcolor='tab:orange')
     ax2.legend(loc='lower right')
     fighg.tight_layout()
@@ -156,12 +184,15 @@ def find_Hg_peaks(lam_Hg, A_Hg, Hg_lines):
     fighg.savefig(f'Plots/Hg_line_fit.pdf', bbox_inches="tight")
     plt.show()
 
-    print(results_line_hg.fit_report())
-    print(f"a = {print_round_val(results_line_hg.params['a'].value, results_line_hg.params['a'].stderr)} nm, b = {print_round_val(results_line_hg.params['b'].value, results_line_hg.params['b'].stderr)} nm/s")
 
     return peaks_hg, line_peaks, results_line_hg, lambda_peaks
 
 peaks_hg, line_peaks, results_line_hg, lambda_peaks = find_Hg_peaks(lam_Hg, A_Hg, Hg_lines)
+
+lambda_peaks_corr = results_line_hg.eval(x=lambda_peaks)
+
+for key in data.keys():
+    data[key]['lambda'] = results_line_hg.eval(x=data[key]['lambda'])
 
 # --------------------------------------------------------------------------------
 # --- Plotting the Hg Lines ------------------------------------------------------
@@ -186,6 +217,7 @@ s_peaks = DatasetSpec(
     marker="x",
     line="None",
 )
+
 s_gauss = DatasetSpec(
     x=lambda_peaks,
     y=np.array([peak[1] for peak in line_peaks]),
@@ -196,9 +228,9 @@ s_gauss = DatasetSpec(
 
 plot_data(
     [s_hg, s_peaks, s_gauss],
-    title=f'Ruby Data: Hg',
-    xlabel='Wavelength (nm)',
-    ylabel='Amplitude (normalized)',
+    title=f'Quecksilber Emissionslinien mit gefundenen Peaks',
+    xlabel='$\\lambda_{nom}$ (nm)',
+    ylabel='Intensität I (normalisiert)',
     filename=f'Plots/Hg_lines.pdf',
     height=15,
     color_seed=89,
@@ -225,6 +257,7 @@ data['diff_B2']['A'] *= 10.0
 
 
 s_all = []
+s_all_N = []
 for key in sorted(data.keys()):
     if key == 'Hg_lines':
         continue
@@ -238,7 +271,16 @@ for key in sorted(data.keys()):
         marker='None',
         line='-',
     )
+    mask = data[key]['lambda'] >= 670.0
+    s_N = DatasetSpec(
+        x=data[key]['lambda'][mask],
+        y=data[key]['A'][mask],
+        label=key,
+        marker='None',
+        line='-',
+    )
     s_all.append(s)
+    s_all_N.append(s_N)
 
 
 plot_data(
@@ -247,6 +289,20 @@ plot_data(
     xlabel='$\\lambda (nm)$',
     ylabel='Amplitude I',
     filename=f'Plots/All.pdf',
+    color_seed=87,
+    plot=False
+)
+
+
+
+
+
+plot_data(
+    s_all_N,
+    title=f'All Spectra (>= 670 nm)',
+    xlabel='$\\lambda (nm)$',
+    ylabel='Amplitude I',
+    filename=f'Plots/All_N.pdf',
     color_seed=87,
     plot=False
 )
@@ -261,7 +317,6 @@ def find_lambda_shift(lam_ref, I_ref, lam, I, lam_range=None, max_shift=1.0):
     s = np.argsort(lam_ref); lam_ref, I_ref = lam_ref[s], I_ref[s]
     s = np.argsort(lam);     lam,     I     = lam[s],     I[s]
 
-
     if lam_range is not None:
         lo, hi = lam_range
         mask_ref = (lam_ref >= lo) & (lam_ref <= hi)
@@ -272,7 +327,6 @@ def find_lambda_shift(lam_ref, I_ref, lam, I, lam_range=None, max_shift=1.0):
     # normalize Data so that median is 0 and peak-to-peak amplitude is 1
     I_ref = (I_ref - np.median(I_ref)) / np.ptp(I_ref)
     I     = (I     - np.median(I))     / np.ptp(I)
-    
     
     # interpolating functions for both so that we can evaluate at common points
     f_ref = interp1d(lam_ref, I_ref, kind='linear', bounds_error=False, fill_value=np.nan)
@@ -299,36 +353,76 @@ def find_lambda_shift(lam_ref, I_ref, lam, I, lam_range=None, max_shift=1.0):
 
 
 
-ref_key = 'Xe_B'
-lam_ref = data[ref_key]['lambda']
-I_ref   = data[ref_key]['A']
 
+shifts = {key: 0.0 for key in data.keys()}  # cumulative shifts for each spectrum
+
+# 1) Align Xe_B to Xe_N in 670–715 (Xe_N is the absolute reference)
+shift_XeB = find_lambda_shift(data['Xe_N']['lambda'], data['Xe_N']['A'],
+                              data['Xe_B']['lambda'], data['Xe_B']['A'],
+                              lam_range=(670, 715),
+                              max_shift=2.0)
+
+
+data['Xe_B']['lambda'] += shift_XeB
+shifts['Xe_B'] += shift_XeB
+
+# 2) Align all BROAD spectra (*_B) to Xe_B in 450–500
 for key in sorted(data.keys()):
-    if key == 'Hg_lines':
+    if not key.endswith('_B') and not key.endswith('_B2') and not key.endswith('_Basis'):
         continue
+    if key in ['Xe_B', 'Hg_lines']:
+        continue
+
     lam = data[key]['lambda']
     I   = data[key]['A']
-    if key == ref_key:
-        continue
-    if np.min(lam) < 600:
-        shift = find_lambda_shift(lam_ref, I_ref, lam, I,
-                                lam_range=(450, 500),  
-                                max_shift=2.0) 
-    else:
-        shift = find_lambda_shift(data[key.split('_')[0]+'_B']['lambda'], data[key.split('_')[0]+'_B']['A'], lam, I,
-                                lam_range=(670, 715),  
-                                max_shift=2.0) 
+
+    shift = find_lambda_shift(data['Xe_B']['lambda'], data['Xe_B']['A'],
+                              lam, I,
+                              lam_range=(450, 500),
+                              max_shift=2.0)
+
     data[key]['lambda'] += shift
-    print(f"Applied λ shift {shift:.3f} nm to {key}")
+    shifts[key] += shift
     
-    
-    
+
+# 3) Align all NARROW spectra (*_N) to their own *_B in 670–715
+for key in sorted(data.keys()):
+    if not key.endswith('_N') and not key.endswith('_N2'):
+        continue
+    if key == 'Xe_N':   # Xe_N is the reference, do NOT shift
+        continue
+
+    broad_key = key.split('_')[0] + '_B'
+
+    lam = data[key]['lambda']
+    I   = data[key]['A']
+
+    shift = find_lambda_shift(data[broad_key]['lambda'], data[broad_key]['A'],
+                              lam, I,
+                              lam_range=(670, 715),
+                              max_shift=1.0)
+
+    data[key]['lambda'] += shift
+    shifts[key] += shift
+
+# Print shifts as a table
+data_shifts = {key: [shift] for key, shift in shifts.items() if key != 'Hg_lines'}
+data_shifts = dict(sorted(data_shifts.items()))
+
+headers_shifts = {key: {'label': f'{{ $ {key.split('_')[0] + "_{" + key.split('_')[1] + "}" } $}}', 'intermed': True} for key in data_shifts.keys()}
+
+print_standard_table(
+    data_shifts,
+    headers_shifts,
+    caption="Wellenlängenverschiebungen der Spektren nach der Ausrichtung, basierend auf der Minimierung der mittleren quadratischen Abweichung. Vergleichsspektrum ist das $Xe_N$ Spektrum.",
+    label="tab:ruby_wavelength_shifts",
+    show=True
+)
+
 s_all = []
 for key in sorted(data.keys()):
     if key == 'Hg_lines':
         continue
-    prefix = key.split('_')[0]
-    second = key.split('_')[1]
     s = DatasetSpec(
         x=data[key]['lambda'],
         y=data[key]['A'],
@@ -337,7 +431,6 @@ for key in sorted(data.keys()):
         line='-',
     )
     s_all.append(s)
-
 
 plot_data(
     s_all,
@@ -451,24 +544,25 @@ lam_B, T_B, I_Ruby_B, I_Xe_B2 = compute_ratio(data['Ruby_B']['lambda'], data['Ru
 plot_data(
     [DatasetSpec(x=data['diff_B']['lambda'], y=data['diff_B']['A'], label='diff_B', marker='None', line='-'),
      DatasetSpec(x=data['Xe_B']['lambda'], y=data['Xe_B']['A'], label='Xe_B', marker='None', line='-'),
-     DatasetSpec(x=lam_diff_B, y=I_diff_B, label='diff_B resampled', marker='None', line='-'),
-     DatasetSpec(x=lam_diff_B, y=I_Xe_B, label='Xe_B resampled', marker='None', line='-')],
-    title=f'Ruby Data: Difference',
-    xlabel='Wavelength (nm)',
-    ylabel='Amplitude I',
+     DatasetSpec(x=data['Ruby_B']['lambda'], y=data['Ruby_B']['A'], label='Ruby_B', marker='None', line='-')],
+    title=f'Vergleich der Spektren diff_B, Xe_B und Ruby_B',
+    xlabel='$\\lambda$ (nm)',
+    ylabel='Intensität I',
+    filename=f'Plots/Raw_Ruby_Diff.pdf',
     height=15,
-    plot=True
+    plot=False
 )
 
 lam = np.linspace(350, 715, 10000)
 plot_data(
     [DatasetSpec(x=lam_diff_B, y=1 - I_diff_B/I_Xe_B, label='diff_B / Xe_B', marker='None', line='-', fit_x=lam, fit_y=make_splrep(lam_diff_B, 1 - I_diff_B/I_Xe_B, s=0.5)(lam), fit_color='orange'),
      DatasetSpec(x=lam_B, y=T_B, label='Ruby_B / Xe_B', marker='None', line='-')],
-    title=f'Ruby Data: Difference',
-    xlabel='Wavelength (nm)',
-    ylabel='Transmittance T',
+    title=f'Transmittanzspektren von Rubin und Differenzspektrum',
+    xlabel='$\\lambda$ (nm)',
+    ylabel='Transmittanz T',
+    filename=f'Plots/Transmittance_Ruby_Diff.pdf',
     height=15,
-    plot=True
+    plot=False
 )
 
 initial_T_1 = 408.0
@@ -495,15 +589,15 @@ print(results_diff_T_2.fit_report())
 
 const = 10**7 / B_val
 
-print(f"Resulting centers: ^4 T_1 fit = {results_diff_T_1.params['b'].value:.2f} ± {results_diff_T_1.params['b'].stderr:.2f}, ^4 T_2 fit = {results_diff_T_2.params['b'].value:.2f} ± {results_diff_T_2.params['b'].value:.2f}")
-print(f"Resulting normed Emergies by B: ^4 T_1 = {const/ results_diff_T_1.params['b'].value:.3f} ± {const * results_diff_T_1.params['b'].stderr / (results_diff_T_1.params['b'].value**2):.3f}, ^4 T_2 = {const/ results_diff_T_2.params['b'].value:.3f}  ± {const * results_diff_T_2.params['b'].value / (results_diff_T_2.params['b'].value**2):.3f} with ratio {results_diff_T_2.params['b'].value / results_diff_T_1.params['b'].value:.4f}")
+print(f"Resulting centers: $\\lambda_{{^4 T_1}} = ({results_diff_T_1.params['b'].value:.2f} \\pm {results_diff_T_1.params['b'].stderr:.2f})$ nm, $\\lambda_{{^4 T_2}} = ({results_diff_T_2.params['b'].value:.2f} \\pm {results_diff_T_2.params['b'].stderr:.2f})$ nm")
+print(f"Resulting normed Emergies by B: $E_{{^4 T_1}} = ({const/ results_diff_T_1.params['b'].value:.3f} \\pm {const * results_diff_T_1.params['b'].stderr / (results_diff_T_1.params['b'].value**2):.3f})$, $E_{{^4 T_2}} = ({const/ results_diff_T_2.params['b'].value:.3f}  \\pm {const * results_diff_T_2.params['b'].stderr / (results_diff_T_2.params['b'].value**2):.3f})$ with ratio {results_diff_T_2.params['b'].value / results_diff_T_1.params['b'].value:.4f}")
 
 
 plot_data(
     [s_B],
-    title=f'Ruby Data: Absorbance Ruby / Xe Spectra',
-    xlabel='Wavelength (nm)',
-    ylabel='Absorbance (A)',
+    title=f'Absorptionsspektrum von Rubin',
+    xlabel='$\\lambda$ (nm)',
+    ylabel='Absorbanz (A)',
     filename=f'Plots/Abs_Ruby_Xe.pdf',
     height=15,
     plot=False
@@ -536,9 +630,26 @@ x = np.linspace(0, 4.154, 1000)
 E_T1, dE_T1, Dq_T1, spline_T1, s_T1 = build_level('$^4 T_1$', spline_T1, results_diff_T_1, const, x)
 E_T2, dE_T2, Dq_T2, spline_T2, s_T2 = build_level('$^4 T_2$', spline_T2, results_diff_T_2, const, x)
 
-print(f"Fitted Energy Levels by B: E_T1 = {E_T1:.3f} ± {dE_T1:.3f} at Dq/B = {Dq_T1:.3f}, E_T2 = {E_T2:.3f} ± {dE_T2:.3f} at Dq/B = {Dq_T2:.3f}, with ratio E_T2 / E_T1 = {E_T2 / E_T1:.4f}")
+# Determine asymmetric Dq/B errors by inverting at E ± dE
+Dq_T1_plus  = fsolve(lambda Dq: spline_T1(Dq) - (E_T1 + dE_T1), x0=Dq_T1)[0]
+Dq_T1_minus = fsolve(lambda Dq: spline_T1(Dq) - (E_T1 - dE_T1), x0=Dq_T1)[0]
+dDq_T1_up   = max(0.0, Dq_T1_plus  - Dq_T1)
+dDq_T1_dn   = max(0.0, Dq_T1       - Dq_T1_minus)
+
+Dq_T2_plus  = fsolve(lambda Dq: spline_T2(Dq) - (E_T2 + dE_T2), x0=Dq_T2)[0]
+Dq_T2_minus = fsolve(lambda Dq: spline_T2(Dq) - (E_T2 - dE_T2), x0=Dq_T2)[0]
+dDq_T2_up   = max(0.0, Dq_T2_plus  - Dq_T2)
+dDq_T2_dn   = max(0.0, Dq_T2       - Dq_T2_minus)
+
+print(f"Fitted Energy Levels by B:\n"
+    f"E_T1 = {E_T1:.3f} ± {dE_T1:.3f} at Dq/B = {Dq_T1:.5f} (+{dDq_T1_up:.5f}/-{dDq_T1_dn:.5f}),\n "
+    f"E_T2 = {E_T2:.3f} ± {dE_T2:.3f} at Dq/B = {Dq_T2:.5f} (+{dDq_T2_up:.5f}/-{dDq_T2_dn:.5f}),\n"
+    f"with ratio E_T2 / E_T1 = {E_T2 / E_T1:.4f}")
 print(f"Resulting 10 Dq from fits: 10 Dq = {10* Dq_T1 * B_val:.2f} cm^-1 or {10 * Dq_T1 * B_val * 1.23981e-4:.4f} eV from T1, 10 Dq = {10 * Dq_T2 * B_val:.2f} cm^-1 or {10 * Dq_T2 * B_val * 1.23981e-4:.4f} eV from T2")
 
+print(f"Resultierendes 10 Dq mit Fehlern aus dem Intervall mit durchschnittlichen Abweichungen:\n"
+    f"10 Dq = {10* np.mean([Dq_T1, Dq_T2]) * B_val:.2f} cm^-1 \\pm {np.abs(10 * (Dq_T1 - Dq_T2) * B_val / 2):.2f} cm^{{-1}} \n"
+    f"oder {10 * np.mean([Dq_T1, Dq_T2]) * B_val * 1.23981e-4:.4f} eV \\pm {np.abs(10 * (Dq_T1 - Dq_T2) * B_val * 1.23981e-4 / 2):.4f} eV")
 
 plot_data(
     [s_T1, s_T2],
@@ -608,7 +719,7 @@ plot_data(
     xlabel='Wavelength (nm)',
     ylabel='Intensity I',
     height=15,
-    plot=True
+    plot=False
 )
 
 
